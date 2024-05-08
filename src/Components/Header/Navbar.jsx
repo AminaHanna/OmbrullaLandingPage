a6efbf209974f5e4b989b5a55adc872b31b3acb0
Amina Hanna <aminahanna2002@gmail.com>
2024-05-09 02:15:12 +0530

    dropdown added

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
     <nav className="">
       <div className="pt-8">
-        <div className="flex mx-6 my-5 sm:mx-28 sm:my-8 items-center justify-between h-16">
+        <div className="flex mx-6 sm:mx-28 sm:my-8 items-center justify-between h-16">
             {/* <div className="h-[100px] w-[150px]"> */}
                 <img className='h-[40px]' src={img} alt="Loading..." />
             {/* </div> */}
@@ -16,8 +16,21 @@ const NavBar = () => {
           <div className="hidden md:block">
             <div className="sm:flex sm:gap-8">
                 <Link to="/home"><p className='sm:font-bold sm:text-blue-500'>Home</p></Link>
-                <Link to="/services"><p className='sm:font-bold'>Services <span className=''><i class="fa-solid fa-angle-down"></i></span></p></Link>
-                <Link to="/product"><p className='sm:font-bold'>Product <span className=''><i class="fa-solid fa-angle-down"></i></span></p></Link>
+                <Link to="/services"><p className='dropdown sm:font-bold'>Services <span className=''><i class="fa-solid fa-angle-down"></i></span>
+                  <div className="dropdown-content">
+                    <p className='border-b-2 py-2'>AI Visual Inspection</p>
+                    <p className='border-b-2 py-2'>AI Infrastructure Inspection</p>
+                    <p className='border-b-2 py-2'>AI People Tracking</p>
+                    <p className='py-2'>AI Data Analytics</p>
+                  </div>
+                  </p>
+                </Link>
+                <Link to="/product"><p className='dropdown sm:font-bold'>Product <span className=''><i class="fa-solid fa-angle-down"></i></span>
+                  <div className="dropdown-content">
+                    <p className='py-2'>Asset Performance Management</p>
+                  </div>
+                  </p>
+                </Link>
                 <Link to="/about"><p className='sm:font-bold'>About</p></Link>
                 <Link to="/blog"><p className='sm:font-bold'>Blog</p></Link>
                 <Link to="/contact"><p className='sm:font-bold'>Contact</p></Link>
@@ -38,7 +51,7 @@ const NavBar = () => {
       {isOpen && (
         <div className="md:hidden absolute">
           <div className="pt-2 pb-3 sm:px-3">
-            <div className="w-[250px] bg-white">
+            <div className="w-[250px] bg-white h-[500px]">
                 {/* <div className=" bg-slate-400 p-10"> */}
                     {/* <img className='h-[40px]' src={img} alt="Loading..." /> */}
                 {/* </div> */}
